daea5708a5ec9e6930f96b3cf3909bd3980c2360
Clems-web <clement.servais1@gmail.com>
2020-12-14 11:34:51 +0100

    Finito !

---
+++ exo.js
@@ -0,0 +1,46 @@
+let exo1 = Math.floor( 34.589456);
+console.log("exo 1 : " + exo1);
+
+let exo2 = Math.floor(68.412784);
+console.log("exo 2 : " + exo2);
+
+let exo3 = Math.ceil(39.456);
+console.log("exo 3 "+ exo3);
+
+let exo4 = Math.ceil(27.34876541);
+console.log("exo 4 : " + exo4);
+
+let exo5 = Math.round(5.68);
+console.log("exo 5 : " + exo5);
+
+let exo6 = Math.round(5.41);
+console.log("exo 6 : " + exo6);
+
+let exo7 = Math.trunc(6.12457);
+console.log("exo 7 : " + exo7);
+
+let exo8 = Math.trunc(9.568124);
+console.log("exo 8 : " + exo8);
+
+let exo9 = Math.min(5, 1, 12, -1, 45);
+console.log("exo9 : " + exo9);
+
+let exo10 = Math.min(9, 12, 16, 44, 3);
+console.log("exo10 : " + exo10);
+
+let exo11 = Math.max(5, 1, 12, -1, 45);
+console.log("exo11 : " + exo11);
+
+let exo12 = Math.max(9, 12, 16, 44, 3);
+console.log("exo12 : " + exo12);
+
+console.log("exo 13 : " + Math.abs(-6));
+
+console.log("exo 14 : " + Math.abs(-56));
+
+
+
+
+
+
+
